8a860679b314b718e738a752fc5fa594e414bc5e
i-shinya <31563549+i-shinyausers.noreply.github.com>
2023-02-09 10:00:21 +0900

    feat: プロンプト削除ボタン追加

--- app/src/components/molecules/PromptEditor.vue
+++ app/src/components/molecules/PromptEditor.vue
@@ -52,6 +52,11 @@ const addSpell = () => {
   emits("send-val", prompts.value);
 };
 
+const deletePrompt = (id: number) => {
+  const res = prompts.value.filter((val) => val.id !== id);
+  emits("send-val", res);
+};
+
 onMounted(() => {
   prompts.value = props.prompt ?? "";
 });
@@ -71,40 +76,49 @@ watch(
     <div class="editor-area">
       <template v-for="item of prompts" key="id">
         <div class="editor-row mb-1">
-          <div class="up-down-icon mr-4">
-            <font-awesome-icon icon="fa-solid fa-angle-up" />
-            <font-awesome-icon icon="fa-solid fa-angle-down" />
-          </div>
-          <Input
-            class="prompt-input mr-4"
-            :id="item.id"
-            :label="null"
-            :value="item.spell"
-            @send-val="receiveVal"
-          ></Input>
-          <div class="emphasis-area">
-            <font-awesome-icon
-              class="mr-2"
-              icon="fa-solid fa-arrow-down-wide-short"
-              @click="restraint(item.id)"
-            />
-            <font-awesome-icon
-              class="mr-2"
-              icon="fa-solid fa-arrow-up-wide-short"
-              @click="emphasis(item.id)"
-            />
-            <div class="emphasis">
-              <template v-if="item.emphasis >= 0">
-                <div>{}</div>
-                <div>&times;</div>
-                <div>{{ item.emphasis }}</div>
-              </template>
-              <template v-else>
-                <div>[]</div>
-                <div>&times;</div>
-                <div>{{ -item.emphasis }}</div>
-              </template>
+          <div class="editor-border-area">
+            <div class="up-down-icon mr-4">
+              <font-awesome-icon icon="fa-solid fa-angle-up" />
+              <font-awesome-icon icon="fa-solid fa-angle-down" />
             </div>
+            <Input
+              class="prompt-input mr-4"
+              :id="item.id"
+              :label="null"
+              :value="item.spell"
+              @send-val="receiveVal"
+            ></Input>
+            <div class="emphasis-area">
+              <font-awesome-icon
+                class="mr-2"
+                icon="fa-solid fa-arrow-down-wide-short"
+                @click="restraint(item.id)"
+              />
+              <font-awesome-icon
+                class="mr-2"
+                icon="fa-solid fa-arrow-up-wide-short"
+                @click="emphasis(item.id)"
+              />
+              <div class="emphasis">
+                <template v-if="item.emphasis >= 0">
+                  <div>{}</div>
+                  <div>&times;</div>
+                  <div>{{ item.emphasis }}</div>
+                </template>
+                <template v-else>
+                  <div>[]</div>
+                  <div>&times;</div>
+                  <div>{{ -item.emphasis }}</div>
+                </template>
+              </div>
+            </div>
+          </div>
+          <div class="close-icon ml-2">
+            <font-awesome-icon
+              class="window-operate-button"
+              icon="fa-solid fa-xmark"
+              @click="deletePrompt(item.id)"
+            />
           </div>
         </div>
       </template>
@@ -134,32 +148,48 @@ watch(
   .editor-area {
     .editor-row {
       display: flex;
-      border: 1px solid rgb(129, 129, 129);
-      background-color: rgb(54, 54, 54);
       align-items: center;
-      padding-left: 4px;
-      padding-right: 4px;
 
-      .prompt-input {
-        flex-grow: 1;
-      }
-      .emphasis-area {
+      .editor-border-area {
         display: flex;
-        flex-grow: 0;
+        border: 1px solid rgb(129, 129, 129);
+        background-color: rgb(54, 54, 54);
+        padding-left: 4px;
+        padding-right: 4px;
         align-items: center;
-
-        .emphasis {
+        flex-grow: 1;
+        .prompt-input {
+          flex-grow: 1;
+        }
+        .emphasis-area {
           display: flex;
+          flex-grow: 0;
           align-items: center;
+
+          .emphasis {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+        }
+        .up-down-icon {
+          display: flex;
+          flex-flow: column;
+          font-size: 14px;
+          flex-grow: 0;
           justify-content: center;
         }
       }
-      .up-down-icon {
+
+      .close-icon {
         display: flex;
-        flex-flow: column;
-        font-size: 14px;
-        flex-grow: 0;
         justify-content: center;
+        align-items: center;
+        height: 20px;
+        width: 20px;
+        border: 1px solid rgb(255, 255, 255);
+        border-radius: 50%;
+        cursor: pointer;
       }
     }
 
